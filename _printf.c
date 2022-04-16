05a203ef1b9c220c661644626b17534d55c62417
Adebobola Adereti <emmanueladebobola132@gmail.com>
2022-04-16 22:58:36 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,8 +4,8 @@
 
 /**
  *_printf - produces output according to a format
- *@format - format string containing the characters and the specifiers
- * Return - length of the formatted output string
+ *@format: format string containing the characters and the specifiers
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
