e1758ccd1d065364e3954637eec4eec9e29b840a
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-16 22:16:31 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include <stdarg.h>
 
+int _printf(const char *format, ...)
+{
+	va_list start;
+
+	va_start (start, format);
+	int f = 0;
+	int l = 0;
+	char *t;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				putchar(va_arg(start, int));
+				f++;
+			}
+			else if (*format == 's')
+			{
+				t = va_arg(start, char*);
+				while (t[l] != '\0')
+				{
+					putchar(t[l]);
+					l++;
+					f++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				f++;
+			}
+			format++;
+		}
+		putchar(*format);
+		format++;
+		f++;
+	}
+	va_end (start);
+	return (f);
+}
