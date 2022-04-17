af1ae76a8b34f4e87e0d87c13a21b1f60296201e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-17 22:52:42 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -8,12 +8,13 @@
  *
  * Return: the number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	int char_count = 0; /* Total number of chars printed to stdout */
 	va_list ap; /* Contains the list of arguments passed after format */
 	int i; /* Used to loop through all characters in format */
+	int l = 0;
+	char *t;
 
 	va_start(ap, format);
 
@@ -24,17 +25,37 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			_putchar(format[i]);
+			putchar(format[i]);
 			char_count++;
 			continue;
 		}
-
+		if (format[i] == '%')
+                {
+                        i++;
+                        if (format[i] == 'c')
+                        {
+                                putchar(va_arg(ap, int));
+                                char_count++;
+                        }
+                        else if (format[i] == 's')
+                        {
+                                t = va_arg(ap, char*);
+                                while (t[l] != '\0')
+                                {
+                                        putchar(t[l]);
+                                        l++;
+                                        char_count++;
+                                }
+                        }
+                        else if (format[i] == '%')
+                        {
+                                putchar('%');
+                                char_count++;
+                        }
+                }
 		if (format[i + 1] == '\0')
 		{
-			return (-1);
+			return (char_count);
 		}
-
-		i++;
 	}
-	return (char_count);
 }
