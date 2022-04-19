b35bed3f89e15dc9c0c37c89fbe69d9f96cc8246
Adebobola Adereti <emmanueladebobola132@gmail.com>
2022-04-19 20:50:53 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -26,6 +26,7 @@ int printIdentifiers(char next, va_list arg)
 		{"S", print_STR},
 		{NULL, NULL}
 	};
+
 	for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
 	{
 		if (functs[functsIndex].indentifier[0] == next)
@@ -42,14 +43,17 @@ int printIdentifiers(char next, va_list arg)
  * (excluding the null byte used to end output to strings)
  * return -1 for incomplete identifier error
  */
+
 int _printf(const char *format, ...)
 {
 	unsigned int i;
 	int identifierPrinted = 0, charPrinted = 0;
 	va_list arg;
+
 	va_start(arg, format);
 	if (format == NULL)
 		return (-1);
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -67,11 +71,13 @@ int _printf(const char *format, ...)
 		}
 		if (format[i + 1] == '\0')
 			return (-1);
+
 		identifierPrinted = printIdentifiers(format[i + 1], arg);
 		if (identifierPrinted == -1 || identifierPrinted != 0)
 			i++;
 		if (identifierPrinted > 0)
 			charPrinted += identifierPrinted;
+
 		if (identifierPrinted == 0)
 		{
 			_putchar('%');
